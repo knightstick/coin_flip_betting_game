26dc346f10302e0369e480737d8aea47dc84f3d0
Chris Jewell <chrisjohnjewell@gmail.com>
2019-05-19 16:48:43 +1000

    Add cash_out to TableSession

--- lib/boundary/table_session.ex
+++ lib/boundary/table_session.ex
@@ -22,6 +22,12 @@ defmodule CoinFlipBettingGame.Boundary.TableSession do
     {:reply, table.bets.stakes, table}
   end
 
+  def handle_call({:cash_out, player}, _from, table) do
+    table = Table.cash_out(table, player)
+    # TODO: return amount of money cashed out
+    {:reply, 0, table}
+  end
+
   def join_table(session, player) do
     GenServer.call(session, {:join_table, player})
   end
@@ -32,6 +38,10 @@ defmodule CoinFlipBettingGame.Boundary.TableSession do
 
   def flip_and_pay(session) do
     GenServer.call(session, :flip_and_pay)
+  end
+
+  def cash_out(session, player) do
+    GenServer.call(session, {:cash_out, player})
   end
 
   defp default_stake() do
