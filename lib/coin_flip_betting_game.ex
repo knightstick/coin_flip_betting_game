c70785ddabb25129f040944d3d7afac235f4c5c6
Chris Jewell <chrisjohnjewell@gmail.com>
2019-05-19 18:36:23 +1000

    Wire up all TableSession actions in top level module

--- lib/coin_flip_betting_game.ex
+++ lib/coin_flip_betting_game.ex
@@ -4,4 +4,16 @@ defmodule CoinFlipBettingGame do
   def join_table(table_name, player) do
     TableSession.join_or_create(table_name, player)
   end
+
+  def bet(table_name, player, {_, _} = bet) do
+    TableSession.bet(table_name, player, bet)
+  end
+
+  def flip_and_pay(table_name) do
+    TableSession.flip_and_pay(table_name)
+  end
+
+  def cash_out(table_name, player) do
+    TableSession.cash_out(table_name, player)
+  end
 end
