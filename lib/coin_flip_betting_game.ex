d7d541ade93ecd02e1272c418a58cbae5bdf58e6
Chris Jewell <chrisjohnjewell@gmail.com>
2019-05-19 17:11:06 +1000

    Remove pre-generated test code

--- lib/coin_flip_betting_game.ex
+++ lib/coin_flip_betting_game.ex
@@ -1,18 +1,2 @@
 defmodule CoinFlipBettingGame do
-  @moduledoc """
-  Documentation for CoinFlipBettingGame.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CoinFlipBettingGame.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/coin_flip_betting_game_test.exs
+++ test/coin_flip_betting_game_test.exs
@@ -1,8 +1,4 @@
 defmodule CoinFlipBettingGameTest do
   use ExUnit.Case
   doctest CoinFlipBettingGame
-
-  test "greets the world" do
-    assert CoinFlipBettingGame.hello() == :world
-  end
 end
