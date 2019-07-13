ffc6c22c5902256d62e54b7ae5753b1d58cb9c50
Chris Jewell <chrisjohnjewell@gmail.com>
2019-07-13 15:14:41 +1000

    Specify elixir 1.9.0

--- apps/coin_flip_betting_game_interface/mix.exs
+++ apps/coin_flip_betting_game_interface/mix.exs
@@ -5,7 +5,7 @@ defmodule CoinFlipBettingGameInterface.MixProject do
     [
       app: :coin_flip_betting_game_interface,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
